c7f391067da597bbd8fa34a9e0d1ab7b9de8c88b
Ali Raheem <ali@theraheemfamily.co.uk>
2017-02-08 13:22:46 +0000

    Initial commit

---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject braille-web "0.1.0-SNAPSHOT"
+  :description "website with a RESTful api to decode braille."
+  :url ""
+  :min-lein-version "2.0.0"
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [compojure "1.5.1"]
+                 [ring/ring-defaults "0.2.1"]]
+  :plugins [[lein-ring "0.9.7"]]
+  :ring {:handler braille-web.handler/app}
+  :profiles
+  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                        [ring/ring-mock "0.3.0"]]}})
