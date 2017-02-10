8d072baf3a9d55c9cc49a70447a3880676b4c00b
Ali Raheem <ali@theraheemfamily.co.uk>
2017-02-10 23:36:54 +0000

    Functional tests added

--- test/braille_web/handler_test.clj
+++ test/braille_web/handler_test.clj
@@ -7,7 +7,11 @@
   (testing "main route"
     (let [response (app (mock/request :get "/"))]
       (is (= (:status response) 200))))
-  
+  (testing "decode"
+    (let [response (app (mock/request :post "/decode" {"data" "1"}))]
+      (is (= (:status response) 200))
+      (is (= (clojure.string/includes?  (:body response) "<textarea>a</")))
+      (is (= (clojure.string/includes?  (:body response) "Ali Raheem")))))
   (testing "not-found route"
     (let [response (app (mock/request :get "/invalid"))]
       (is (= (:status response) 404)))))
